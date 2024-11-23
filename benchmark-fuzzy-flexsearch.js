7443a722259f05596c98d2d9adb8fce0707c5f52
Ashesh Vidyut <ashesh.vidyut@hashicorp.com>
2024-11-23 18:54:05 +0800

    benchmark flexsearch.js

---
+++ benchmark-fuzzy-flexsearch.js
@@ -0,0 +1,32 @@
+import FlexSearch from "flexsearch";
+
+// Configure FlexSearch for fuzzy search
+const index = new FlexSearch.Index({
+    tokenize: "full",  // Tokenization strategy
+    resolution: 3,        // Scoring precision
+    threshold: 30000,
+});
+
+// Generate and add the large dataset to the index
+const largeDataset = Array.from({ length: 100000 }, (_, i) => ({
+    id: i,
+    text: `Sample entry number ${i}`,
+}));
+
+console.time("Indexing Time");
+largeDataset.forEach((item) => {
+    index.add(item.id, item.text);
+})
+console.timeEnd("Indexing Time");
+
+// Perform searches and measure search time
+const queries = ["Sample entry number 123", "Sample entry number 5000"];
+queries.forEach((query) => {
+    console.time(`Search time for "${query}"`);
+    const results = index.search(query, {limit: 10000});
+    console.timeEnd(`Search time for "${query}"`);
+    console.log(`Results for "${query}":`, results.length); // Display first 10 results
+    results.forEach((result) => {
+        console.log(result);
+    })
+});
---
+++ benchmark-prefix-flexsearch.js
@@ -0,0 +1,39 @@
+import FlexSearch from "flexsearch";
+
+/*
+ Prefix Based Search Benchmarking
+*/
+
+const dataset = [];
+for (let i = 0; i < 100000; i++) {
+    dataset.push({ id: i, text: `Sample text entry number ${i}` });
+}
+
+const index = new FlexSearch.Index({
+    tokenize: "forward",  // Tokenization strategy
+    threshold: 0,         // Matching threshold
+    resolution: 9,        // Scoring resolution
+    doc: {
+        id: "id",           // Unique identifier for documents
+        field: ["text"],    // Fields to index
+    },
+});
+
+
+console.time("Indexing Time");
+dataset.forEach((item) => {
+    index.add(item.id, item.text);
+});
+console.timeEnd("Indexing Time");
+
+// Perform a search
+console.time("Search Time");
+const results = index.search("Sample text entry number 500", {limit: 10000});
+console.timeEnd("Search Time");
+
+console.log(`Found ${results.length} results.`);
+
+// results.forEach((item) => {
+//     console.log(item)
+// })
+
